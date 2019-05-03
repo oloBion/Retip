d75a75049ef3454438743d32f63b94e380e4e083
Paolo Bonini <45519144+PaoloBnn@users.noreply.github.com>
2019-05-03 19:15:49 +0200

    Update function_prep_mona.R

--- R/function_prep_mona.R
+++ R/function_prep_mona.R
@@ -9,24 +9,29 @@
 
 
 
-  prep.mona2 <- function(mspfile="mona.msp") {
+  prep.mona <- function(mspfile="mona.msp") {
+  specfile <- readLines(mspfile, n=-1L)
+  startind <- grep("Name:", specfile)
+  endind <- c(startind[-1]-1, length(specfile))
 
-    specfile <- readLines(mspfile, n=-1L)
-    startind <- grep("NAME:", specfile)
-    endind <- c(startind[-1]-1, length(specfile))
+  det_list <- lapply(1:length(startind), function(i){
+    spect <- specfile[startind[i]:endind[i]]
+    ik <- gsub("InChIKey: ","",spect[grep("InChIKey: ", spect)])
+    com_vec <- spect[grep("Comments: ",spect)]
 
-    det_list <- lapply(1:length(startind), function(i){
-      spect <- specfile[startind[i]:endind[i]]
-      ik <- gsub("INCHIKEY: ","",spect[grep("INCHIKEY: ", spect)])
+    smi <- gsub("\"|computed SMILES=","",stringi::stri_extract_all_regex(com_vec,"computed SMILES.+?\""))
 
-      cname <- gsub("NAME: ","",spect[1])
-      c(cname,ik)
-    })
-    det_list <- det_list[which(sapply(det_list, length)==2)]
-    det_list.df <- data.frame(do.call(rbind,det_list), stringsAsFactors = F)
-    names(det_list.df) <- c("Name","InChIKey")
-    det_list.df[!duplicated(det_list.df),]
-  }
+    cname <- gsub("Name: ","",spect[1])
+    c(cname,ik,smi)
+  })
+  det_list <- det_list[which(sapply(det_list, length)==3)]
+  det_list.df <- data.frame(do.call(rbind,det_list), stringsAsFactors = F)
+  names(det_list.df) <- c("Name", "InChIKey","SMILES")
+  det_list.df[!duplicated(det_list.df),]
+}
+
+
+
 
 
 
