a577942903013b54ddfc8fca2e977e3d6df4bb24
Ales Krenek <ljocha@ics.muni.cz>
2020-08-04 17:49:19 +0200

    dummy example working

--- Dockerfile
+++ Dockerfile
@@ -1,47 +1,32 @@
 FROM rocker/r-ver:3.6.2
 
-RUN Rscript -e 'install.packages("devtools",repos="https://mirrors.nic.cz/R/")'
+ARG MIRROR=https://mirrors.nic.cz/R/
 
 ENV DEBIAN_FRONTEND=noninteractive
 ENV TZ=Europe/Prague
+
 
 RUN apt -y update
 RUN apt -y install libpng-dev zlib1g-dev openjdk-11-jdk-headless
 RUN apt -y install libpcre3-dev liblzma-dev libbz2-dev
 RUN apt -y install libicu-dev 
  
-RUN Rscript -e 'install.packages(c("rcdklibs", "rJava", "png", "ggplot2", "ModelMetrics" ,"keras", "caret", "xgboost", "doParallel", "brnn", "rcdk", "dplyr"),repos="https://mirrors.nic.cz/R/")'
+RUN apt install -y libssl-dev libxml2-dev libssh2-1-dev libcurl4-openssl-dev
 
-RUN Rscript -e 'install.packages("randomForest",repos="https://mirrors.nic.cz/R/")'
+RUN Rscript -e "install.packages('devtools',repos='${MIRROR}')"
+RUN Rscript -e 'devtools::install_github("Paolobnn/Retiplib")'
+RUN Rscript -e 'devtools::install_github("Paolobnn/Retip")'
 
-# examples need it
-RUN Rscript -e 'install.packages("readxl",repos="https://mirrors.nic.cz/R/")'
-
-
-RUN mkdir -p /build/retip
-WORKDIR /build
-COPY DESCRIPTION NAMESPACE Retip.Rproj /build/retip/
-COPY R/ /build/retip/R/
-COPY data/ /build/retip/data/
-COPY examples/ /build/retip/examples/
-COPY man/ /build/retip/man/
-COPY vignettes/ /build/retip/vignettes/
-
-
-WORKDIR /build/retip
-RUN R CMD build .
-RUN Rscript -e 'install.packages("Retip_0.5.4.tar.gz")'
+RUN Rscript -e "install.packages('readxl',repos='${MIRROR}')"
+RUN Rscript -e 'reticulate::install_miniconda()'
+RUN bash -c "source /root/.local/share/r-miniconda/bin/activate r-reticulate && Rscript -e 'library(keras); install_keras()'"
 
 WORKDIR /
-RUN rm -rf /build
 
 RUN apt -y remove $(dpkg -l | grep -- -dev | awk '{print $2}')
 RUN apt -y remove openjdk-11-jdk-headless
 # nice but removes too much
 # RUN apt -y autoremove
-
-# if we ever need it
-# RUN apt -y install openjdk-11-jre-headless
 
 RUN apt -y clean
 
---
+++ examples/workflow_small.R
@@ -0,0 +1,119 @@
+#options(java.parameters = c("-XX:+UseConcMarkSweepGC", "-Xmx8192m"))
+# options(java.parameters = c("-Xmx1024m"))
+
+library(Retip)
+
+#Starts parallel computing
+prep.wizard()
+
+# import excel file for training and testing data
+RP2 <- readxl::read_excel("Plasma_positive_small.xlsx", sheet = "lib_2", col_types = c("text", 
+                                                                              "text", "text", "numeric"))
+# import excel file for external validation set
+RP_ext <- readxl::read_excel("Plasma_positive_small.xlsx", sheet = "ext", col_types = c("text", 
+                                                                                  "text", "text", "numeric"))
+# Calculate Chemical Descriptors fron RCDK
+descs2 <- getCD(RP2)
+descs_ext <- getCD(RP_ext)
+
+# Clean dataset from NA and low variance value
+db_rt <- proc.data(descs2)
+db_ext <- descs_ext[names(descs_ext) %in% names(db_rt)]
+
+#####################################################################################
+################# Options center and scale data  ####################################
+
+# this option improves Keras model
+
+preProc <- cesc(db_rt) #Build a model to use for center and scale a dataframe 
+db_rt <- predict(preProc,db_rt) # use the above created model for center and scale dataframe
+db_ext <- predict(preProc,db_ext)
+
+# IMPORTANT : if you use this option remember to set cesc variable 
+# #########   into rt.spell and getRT.smile functions
+
+#####################################################################################
+#####################################################################################
+
+
+#Split in training and testing using caret::createDataPartition
+set.seed(101)
+inTraining <- caret::createDataPartition(db_rt$XLogP, p = .8, list = FALSE)
+training <- db_rt[ inTraining,]
+testing  <- db_rt[-inTraining,]
+
+
+
+# Train Model
+
+#xgb <- fit.xgboost(training)
+
+#rf  <- fit.rf(training)
+
+#brnn <- fit.brnn(training)
+
+print('Keras...')
+keras <- fit.keras(training,testing)
+
+#lightgbm <- fit.lightgbm(training,testing)
+
+
+# Plot Variable importance for Xgboost or Random Forest or Brnn using Caret function
+
+#plot(caret::varImp(xgb), top =20, main="Top 20 Variable Impact Plot - XGB - RIKEN PLASMA")
+
+
+# Plot Model
+
+#p.model2(training, m=xgb,title = "RP - TRAINING- XGB",crh_leght = 12)
+
+
+print('Score ...')
+stat <- get.score(db_ext,keras)
+
+
+# save or load model all except keras #
+
+#saveRDS(lightgbm, "light_plasma.rds")
+
+#light_plasma <- readRDS("light_plasma.rds")
+
+
+# save or load model  keras #
+
+print('Save HDF5...')
+save_model_hdf5(keras,filepath = "keras_HI")
+
+print('Load HDF5...')
+keras_HI <- load_model_hdf5("keras_HI")
+
+
+
+#mona
+
+#print('prep.mona ...')
+#db_mona <- prep.mona(msp="MoNA-export-CASMI_2016.msp")
+#
+#mona <- getCD(db_mona)
+#
+#print('RT.spell...')
+#mona_rt3 <- RT.spell(training,mona,model=keras_HI)
+#
+#addRT.mona(msp="MoNA-export-CASMI_2016.msp",mona_rt)
+#
+
+#all_pred <- RT.spell(training,target = "ALL",model=xgb)
+all_pred <- RT.spell(training,target = "ALL",model=keras_HI)
+all_pred$Name <- NULL
+all_pred$SMILES <- NULL
+colnames(all_pred) <- c("INCHKEY","RT")
+write.table(x = all_pred, "all_msfinder_Riken.txt", sep = "\t", col.names = T, row.names=F, dec = ".", quote = F)
+
+
+
+#get individual prediction from single smile
+
+smi <- "C1=CC(=CC=C1C(=O)O)N"
+getRT.smile(smile=smi,training,model=keras)
+
+
