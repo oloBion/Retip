0804a099937967d6eae673f384d39e2158825f0f
Neus Pou Amengual <neuspouamengual@gmail.com>
2024-05-23 16:50:10 +0200

    workflow for one or all models

--- examples/workflow_PlaSMA.R
+++ examples/workflow_PlaSMA.R
@@ -2,7 +2,8 @@
 keras_installed <- TRUE
 remove_descriptors <- TRUE
 cesc <- FALSE
-build_models <- TRUE
+build_models <- FALSE
+model <- "xgb" # all, xgb, rf, lightgbm, keras, brnn, h2o
 
 # Packages import
 setwd("~/Documentos/Retip/examples")
@@ -44,37 +45,83 @@ training <- db_rt[in_training, ]
 testing <- db_rt[-in_training, ]
 
 # Train Model
-if (build_models) {
-  rf  <- fit.rf(training)
-  saveRDS(rf, "rf_model.rds")
+if (model == "all") {
+  if (build_models) {
+    rf  <- fit.rf(training)
+    saveRDS(rf, "rf_model.rds")
 
-  brnn <- fit.brnn(training)
+    brnn <- fit.brnn(training)
+    saveRDS(brnn, "brnn_model.rds")
+
+    keras <- fit.keras(training, testing)
+    save_model_hdf5(keras, filepath = "keras_HI")
+
+    lightgbm <- fit.lightgbm(training, testing)
+    saveRDS(lightgbm, "lightgbm_model.rds")
+
+    xgb <- fit.xgboost(training)
+    saveRDS(xgb, "xgb_model.rds")
+
+    aml <- fit.automl.h2o(training)
+    h2o::h2o.saveModel(aml@leader, "automl_h2o_model")
+  } else  {
+    rf <- readRDS("rf_model.rds")
+    brnn <- readRDS("brnn_model.rds")
+    keras <- keras::load_model_hdf5("keras_HI")
+    lightgbm <- readRDS("lightgbm_model.rds")
+    xgb <- readRDS("xgb_model.rds")
+    h2o::h2o.init(nthreads = -1)
+    aml <- h2o.loadModel("automl_h2o_model/###")
+    # replace ### with the name of your saved model
+  }
+} else if (model == "rf") {
+  if (build_models) {
+    mdl <- fit.rf(training)
+    saveRDS(rf, "rf_model.rds")
+  } else {
+    mdl <- readRDS("rf_model.rds")
+  }
+} else if (model == "brnn") {
+  mdl <- fit.brnn(training)
   saveRDS(brnn, "brnn_model.rds")
-
-  keras <- fit.keras(training, testing)
-  save_model_hdf5(keras, filepath = "keras_HI")
-
-  lightgbm <- fit.lightgbm(training, testing)
-  saveRDS(lightgbm, "lightgbm_model.rds")
-
-  xgb <- fit.xgboost(training)
-  saveRDS(xgb, "xgb_model.rds")
-
-  aml <- fit.automl.h2o(training)
-  h2o::h2o.saveModel(aml@leader, "automl_h2o_model")
-} else {
-  xgb <- readRDS("xgb_model.rds")
-  rf <- readRDS("rf_model.rds")
-  brnn <- readRDS("brnn_model.rds")
-  keras <- keras::load_model_hdf5("keras_HI")
-  lightgbm <- readRDS("lightgbm_model.rds")
-  h2o::h2o.init(nthreads = -1)
-  aml <- h2o.loadModel("automl_h2o_model/###") 
-  # replace ### with the name of your saved model
+} else if (model == "keras") {
+  if (build_models) {
+    mdl <- fit.keras(training, testing)
+    save_model_hdf5(keras, filepath = "keras_HI")
+  } else {
+    mdl <- keras::load_model_hdf5("keras_HI")
+  }
+} else if (model == "lightgbm") {
+  if (build_models) {
+    mdl <- fit.lightgbm(training, testing)
+    saveRDS(lightgbm, "lightgbm_model.rds")
+  } else {
+    mdl <- readRDS("lightgbm_model.rds")
+  }
+} else if (model == "xgb") {
+  print("OK")
+  if (build_models) {
+    mdl <- fit.xgboost(training)
+    saveRDS(xgb, "xgb_model.rds")
+  } else {
+    mdl <- readRDS("xgb_model.rds")
+  }
+} else if (model == "h2o") {
+  if (build_models) {
+    mdl <- fit.automl.h2o(training)
+    h2o::h2o.saveModel(aml@leader, "automl_h2o_model")
+  } else {
+    h2o::h2o.init(nthreads = -1)
+    mdl <- h2o.loadModel("automl_h2o_model/###")
+  }
 }
 
 # Model stats
-stat <- get.score(testing, xgb, rf, brnn, keras, lightgbm, aml)
+if (model == all) {
+  stat <- get.score(testing, xgb, rf, brnn, keras, lightgbm, aml)
+} else {
+  stat <- get.score(testing, mdl)
+}
 print(stat)
 
 # Retention Time Prediction SPELL
@@ -100,26 +147,35 @@ if (cesc) {
 }
 
 # Models
-if (cesc) {
-  rp_ext_pred_xgb <- Retip::RT.spell(training, db_rt_ext, model = xgb,
-                                     cesc = preproc)
-  rp_ext_pred_rf <- Retip::RT.spell(training, db_rt_ext, model = rf,
-                                    cesc = preproc)
-  rp_ext_pred_brnn <- Retip::RT.spell(training, db_rt_ext, model = brnn,
+if (model == all) {
+  if (cesc) {
+    rp_ext_pred_xgb <- Retip::RT.spell(training, db_rt_ext, model = xgb,
+                                       cesc = preproc)
+    rp_ext_pred_rf <- Retip::RT.spell(training, db_rt_ext, model = rf,
                                       cesc = preproc)
-  rp_ext_pred_lightgbm <- RT.spell(training, db_rt_ext, model = lightgbm,
-                                   cesc = preproc)
-  rp_ext_pred_keras <- RT.spell(training, db_rt_ext, model = keras,
+    rp_ext_pred_brnn <- Retip::RT.spell(training, db_rt_ext, model = brnn,
+                                        cesc = preproc)
+    rp_ext_pred_lightgbm <- RT.spell(training, db_rt_ext, model = lightgbm,
+                                     cesc = preproc)
+    rp_ext_pred_keras <- RT.spell(training, db_rt_ext, model = keras,
+                                  cesc = preproc)
+    rp_ext_pred_aml <- RT.spell(training, db_rt_ext, model = aml,
                                 cesc = preproc)
-  rp_ext_pred_aml <- RT.spell(training, db_rt_ext, model = aml,
-                              cesc = preproc)
+  } else {
+    rp_ext_pred_xgb <- Retip::RT.spell(training, rp_ext_desc, model = xgb)
+    rp_ext_pred_rf <- Retip::RT.spell(training, rp_ext_desc, model = rf)
+    rp_ext_pred_brnn <- Retip::RT.spell(training, rp_ext_desc, model = brnn)
+    rp_ext_pred_lightgbm <- RT.spell(training, rp_ext_desc, model = lightgbm)
+    rp_ext_pred_keras <- RT.spell(training, rp_ext_desc, model = keras)
+    rp_ext_pred_aml <- RT.spell(training, rp_ext_desc, model = aml)
+  }
 } else {
-  rp_ext_pred_xgb <- Retip::RT.spell(training, rp_ext_desc, model = xgb)
-  rp_ext_pred_rf <- Retip::RT.spell(training, rp_ext_desc, model = rf)
-  rp_ext_pred_brnn <- Retip::RT.spell(training, rp_ext_desc, model = brnn)
-  rp_ext_pred_lightgbm <- RT.spell(training, rp_ext_desc, model = lightgbm)
-  rp_ext_pred_keras <- RT.spell(training, rp_ext_desc, model = keras)
-  rp_ext_pred_aml <- RT.spell(training, rp_ext_desc, model = aml)
+  if (cesc) {
+    rp_ext_pred <- Retip::RT.spell(training, db_rt_ext, model = mdl,
+                                   cesc = preproc)
+  } else {
+    rp_ext_pred <- Retip::RT.spell(training, rp_ext_desc, model = mdl)
+  }
 }
 
 ## Mona database
@@ -131,62 +187,81 @@ db_mona <- prep.mona(msp = "MoNA-export-CASMI_2016.msp")
 mona <- getCD(db_mona)
 
 # Models
-if (cesc) {
-  mona_rt_xgb <- Retip::RT.spell(training, mona, model = xgb,
-                                 cesc = preproc)
-  mona_rt_rf <- Retip::RT.spell(training, mona, model = rf,
-                                cesc = preproc)
-  mona_rt_brnn <- Retip::RT.spell(training, mona, model = brnn,
+if (model == all) {
+  if (cesc) {
+    mona_rt_xgb <- Retip::RT.spell(training, mona, model = xgb,
+                                   cesc = preproc)
+    mona_rt_rf <- Retip::RT.spell(training, mona, model = rf,
                                   cesc = preproc)
-  mona_rt_lightgbm <- RT.spell(training, mona, model = lightgbm,
-                               cesc = preproc)
-  mona_rt_keras <- RT.spell(training, mona, model = keras,
+    mona_rt_brnn <- Retip::RT.spell(training, mona, model = brnn,
+                                    cesc = preproc)
+    mona_rt_lightgbm <- RT.spell(training, mona, model = lightgbm,
+                                 cesc = preproc)
+    mona_rt_keras <- RT.spell(training, mona, model = keras,
+                              cesc = preproc)
+    mona_rt_aml <- RT.spell(training, mona, model = aml,
                             cesc = preproc)
-  mona_rt_aml <- RT.spell(training, mona, model = aml,
-                          cesc = preproc)
-} else {
-  mona_rt_xgb <- Retip::RT.spell(training, mona, model = xgb)
-  mona_rt_rf <- Retip::RT.spell(training, mona, model = rf)
-  mona_rt_brnn <- Retip::RT.spell(training, mona, model = brnn)
-  mona_rt_lightgbm <- RT.spell(training, mona, model = lightgbm)
-  mona_rt_keras <- RT.spell(training, mona, model = keras)
-  mona_rt_aml <- RT.spell(training, mona, model = aml)
-}
+  } else {
+    mona_rt_xgb <- Retip::RT.spell(training, mona, model = xgb)
+    mona_rt_rf <- Retip::RT.spell(training, mona, model = rf)
+    mona_rt_brnn <- Retip::RT.spell(training, mona, model = brnn)
+    mona_rt_lightgbm <- RT.spell(training, mona, model = lightgbm)
+    mona_rt_keras <- RT.spell(training, mona, model = keras)
+    mona_rt_aml <- RT.spell(training, mona, model = aml)
+  }
 
-addRT.mona(msp = "MoNA-export-CASMI_2016.msp", mona_rt_xgb)
-addRT.mona(msp = "MoNA-export-CASMI_2016.msp", mona_rt_rf)
-addRT.mona(msp = "MoNA-export-CASMI_2016.msp", mona_rt_brnn)
-addRT.mona(msp = "MoNA-export-CASMI_2016.msp", mona_rt_lightgbm)
-addRT.mona(msp = "MoNA-export-CASMI_2016.msp", mona_rt_keras)
-addRT.mona(msp = "MoNA-export-CASMI_2016.msp", mona_rt_aml)
+  addRT.mona(msp = "MoNA-export-CASMI_2016.msp", mona_rt_xgb)
+  addRT.mona(msp = "MoNA-export-CASMI_2016.msp", mona_rt_rf)
+  addRT.mona(msp = "MoNA-export-CASMI_2016.msp", mona_rt_brnn)
+  addRT.mona(msp = "MoNA-export-CASMI_2016.msp", mona_rt_lightgbm)
+  addRT.mona(msp = "MoNA-export-CASMI_2016.msp", mona_rt_keras)
+  addRT.mona(msp = "MoNA-export-CASMI_2016.msp", mona_rt_aml)
+} else {
+  if (cesc) {
+    mona_rt <- Retip::RT.spell(training, mona, model = mdl, cesc = preproc)
+  } else {
+    mona_rt <- Retip::RT.spell(training, mona, model = xgb)
+  }
+
+  addRT.mona(msp = "MoNA-export-CASMI_2016.msp", mona_rt)
+}
 
 ## Retip included library
-
-if (cesc) {
-  all_pred_xgb <- RT.spell(training, target = "ALL", model = xgb,
-                           cesc = preproc)
-  all_pred_rf <- RT.spell(training, target = "ALL", model = rf,
-                          cesc = preproc)
-  all_pred_brnn <- RT.spell(training, target = "ALL", model = brnn,
-                            cesc = preproc)
-  all_pred_lightgbm <- RT.spell(training, target = "ALL", model = lightgbm,
-                                cesc = preproc)
-  all_pred_keras <- RT.spell(training, target = "ALL", model = keras,
+if (model == all) {
+  if (cesc) {
+    all_pred_xgb <- RT.spell(training, target = "ALL", model = xgb,
                              cesc = preproc)
-  all_pred_aml <- RT.spell(training, target = "ALL", model = aml,
-                           cesc = preproc)
-} else {
-  all_pred_xgb <- RT.spell(training, target = "ALL", model = xgb)
-  all_pred_rf <- RT.spell(training, target = "ALL", model = rf)
-  all_pred_brnn <- RT.spell(training, target = "ALL", model = brnn)
-  all_pred_lightgbm <- RT.spell(training, target = "ALL", model = lightgbm)
-  all_pred_keras <- RT.spell(training, target = "ALL", model = keras)
-  all_pred_aml <- RT.spell(training, target = "ALL", model = aml)
-}
+    all_pred_rf <- RT.spell(training, target = "ALL", model = rf,
+                            cesc = preproc)
+    all_pred_brnn <- RT.spell(training, target = "ALL", model = brnn,
+                              cesc = preproc)
+    all_pred_lightgbm <- RT.spell(training, target = "ALL", model = lightgbm,
+                                  cesc = preproc)
+    all_pred_keras <- RT.spell(training, target = "ALL", model = keras,
+                               cesc = preproc)
+    all_pred_aml <- RT.spell(training, target = "ALL", model = aml,
+                             cesc = preproc)
+  } else {
+    all_pred_xgb <- RT.spell(training, target = "ALL", model = xgb)
+    all_pred_rf <- RT.spell(training, target = "ALL", model = rf)
+    all_pred_brnn <- RT.spell(training, target = "ALL", model = brnn)
+    all_pred_lightgbm <- RT.spell(training, target = "ALL", model = lightgbm)
+    all_pred_keras <- RT.spell(training, target = "ALL", model = keras)
+    all_pred_aml <- RT.spell(training, target = "ALL", model = aml)
+  }
 
-export_rtp <- RT.export(all_pred_xgb, program = "MSFINDER", pol = "pos")
-export_rtp <- RT.export(all_pred_rf, program = "MSFINDER", pol = "pos")
-export_rtp <- RT.export(all_pred_brnn, program = "MSFINDER", pol = "pos")
-export_rtp <- RT.export(all_pred_lightgbm, program = "MSFINDER", pol = "pos")
-export_rtp <- RT.export(all_pred_keras, program = "MSFINDER", pol = "pos")
-export_rtp <- RT.export(all_pred_aml, program = "MSFINDER", pol = "pos")
+  export_rtp <- RT.export(all_pred_xgb, program = "MSFINDER", pol = "pos")
+  export_rtp <- RT.export(all_pred_rf, program = "MSFINDER", pol = "pos")
+  export_rtp <- RT.export(all_pred_brnn, program = "MSFINDER", pol = "pos")
+  export_rtp <- RT.export(all_pred_lightgbm, program = "MSFINDER", pol = "pos")
+  export_rtp <- RT.export(all_pred_keras, program = "MSFINDER", pol = "pos")
+  export_rtp <- RT.export(all_pred_aml, program = "MSFINDER", pol = "pos")
+} else {
+  if (cesc) {
+    all_pred <- RT.spell(training, target = "ALL", model = mdl, cesc = preproc)
+  } else {
+    all_pred <- RT.spell(training, target = "ALL", model = mdl)
+  }
+
+  export_rtp <- RT.export(all_pred, program = "MSFINDER", pol = "pos")
+}
